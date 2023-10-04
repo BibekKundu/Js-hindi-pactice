16862e36c9f4f6249c7f36a492a5c871d26891eb
BibekKundu <134617862+BibekKundu@users.noreply.github.com>
2023-10-04 08:05:23 +0000

    variable done

---
+++ basic/variables.js
@@ -0,0 +1,8 @@
+const accountId=12345
+let accountEmail="bibek@gmail.com"
+var accountPassword="12345"
+accountCity="pune"
+let accountState;
+
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
